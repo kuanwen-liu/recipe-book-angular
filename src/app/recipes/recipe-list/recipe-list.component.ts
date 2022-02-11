58ac4adcaf5a9f8ad11d053191aa0399c471fec4
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2022-02-10 21:55:58 -0500

    Add recipe-list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'Test description',
+      'https://omnivorescookbook.com/wp-content/uploads/2022/02/220203_Chinese-Chicken-Liver-with-Onion-and-Pepper_2-500x500.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'Test description',
+      'https://omnivorescookbook.com/wp-content/uploads/2022/02/220203_Chinese-Chicken-Liver-with-Onion-and-Pepper_2-500x500.jpg'
+    ),
+  ];
 
   constructor() {}
 
