c262b8f4a99b75a0c86c277540c8788b431de6ba
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2022-02-13 17:36:46 -0500

    add some functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  page = 'recipe';
+
+  onRecipe(data: string) {
+    this.page = data;
+  }
+
+  onShopping(data: string) {
+    this.page = data;
+  }
+}
