af799fb678739055b466d3638c6eb269ad6b4aa2
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2022-02-14 22:50:01 -0500

    Modify dropdown directive to make the dropdown can also be close by click anywhere outside of the element

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import {
+  Directive,
+  HostBinding,
+  HostListener,
+  ElementRef,
+} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]',
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-
-  @HostListener('click') toggleOpen() {
-    this.isOpen = !this.isOpen;
+  // The dropdown can also be close by click anywhere outside of the element
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    // this.isOpen = !this.isOpen;
+    this.isOpen = this.elRef.nativeElement.contains(event.target)
+      ? !this.isOpen
+      : false;
   }
-
-  constructor() {}
+  constructor(private elRef: ElementRef) {}
 }
