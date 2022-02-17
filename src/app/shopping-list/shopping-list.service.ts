e59363128507372a74043bd0029afabc666d16c8
kuanwen-liu <kuanwen-liu@users.noreply.github.com>
2022-02-16 20:41:22 -0500

    add shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,22 @@
+import { EventEmitter, Injectable } from '@angular/core';
+
 import { Ingredient } from '../shared/ingredient.model';
 
+@Injectable({ providedIn: 'root' })
 export class ShoppingListService {
-  ingredients: Ingredient[] = [
+  ingredientsChange = new EventEmitter<Ingredient[]>();
+
+  private ingredients: Ingredient[] = [
     new Ingredient('Apples', 2),
     new Ingredient('Tomatoes', 1),
   ];
+
+  getIngredients() {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+    this.ingredientsChange.emit(this.ingredients.slice());
+  }
 }
